fd925aa73aec56cdfba863b2f5d825e19092f266
KariNNa33 <103944686+KariNNa33@users.noreply.github.com>
2023-06-08 18:20:32 +0300

    Update controlPano.js

--- js/controlPano.js
+++ js/controlPano.js
@@ -9,7 +9,7 @@ let imageContainer = document.querySelector('.panorama__container');
 
 let SpotPositions = [
     // Связь в коридоре
-    new THREE.Vector3(0, 0, 100)
+    new THREE.Vector3(0, 0, 500)
 ]
 
 const viewer = new PANOLENS.Viewer({
