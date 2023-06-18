d0486a2c9bc8c97aa07cf65e7a23c18bf8feddb2
KariNNa33 <103944686+KariNNa33@users.noreply.github.com>
2023-06-18 15:49:55 +0300

    Update controlPano.js

--- js/controlPano.js
+++ js/controlPano.js
@@ -1,14 +1,13 @@
-const Panorama1 = new PANOLENS.ImagePanorama('assets/Panorama_1.jpg');
-const Panorama2 = new PANOLENS.ImagePanorama('assets/Panorama_2.jpg');
-const Panorama3 = new PANOLENS.ImagePanorama('assets/Panorama_3.jpg');
-const Panorama4 = new PANOLENS.ImagePanorama('assets/Panorama_4.jpg');
-const Panorama5 = new PANOLENS.ImagePanorama('assets/Panorama_5.jpg');
-const Panorama6 = new PANOLENS.ImagePanorama('assets/Panorama_6.jpg');
-const Panorama7 = new PANOLENS.ImagePanorama('assets/Panorama_7.jpg');
+const Panorama1 = new PANOLENS.ImagePanorama('assets/1.jpg');
+const Panorama2 = new PANOLENS.ImagePanorama('assets/2.jpg');
+const Panorama3 = new PANOLENS.ImagePanorama('assets/3.1.jpg');
+const Panorama4 = new PANOLENS.ImagePanorama('assets/4.1jpg');
+const Panorama5 = new PANOLENS.ImagePanorama('assets/5.1.jpg');
+const Panorama6 = new PANOLENS.ImagePanorama('assets/3.2.jpg');
+const Panorama7 = new PANOLENS.ImagePanorama('assets/4.2.jpg');
 let imageContainer = document.querySelector('.panorama__container');
 
 let SpotPositions = [
-    // Связь в коридоре
 // Связь в коридоре
 new THREE.Vector3(1630, 0, -660),
 // Связь коридора_2 и комнаты переговоров
@@ -36,16 +35,12 @@ const viewer = new PANOLENS.Viewer({
 });
 
 Panorama1.link(Panorama2, SpotPositions[0]);
-
+// 1 путь
 Panorama2.link(Panorama3, SpotPositions[1]);
-
 Panorama3.link(Panorama4, SpotPositions[2]);
-
 Panorama4.link(Panorama5, SpotPositions[3]);
-
-Panorama5.link(Panorama6, SpotPositions[4]);
-
+// 2 путь
+Panorama2.link(Panorama6, SpotPositions[4]);
 Panorama6.link(Panorama7, SpotPositions[5]);
-
 
 viewer.add(Panorama1, Panorama2, Panorama3, Panorama4, Panorama5, Panorama6, Panorama7);
